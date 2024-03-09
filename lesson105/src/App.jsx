6264c137a8d4b56eea53f8ef4e27735b597878da
Mert Aktas <mert_aktas07@outlook.com>
2024-03-09 23:10:59 +0300

    useEffect ******

--- lesson105/src/App.jsx
+++ lesson105/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [firstName, setFirstName] = useState("");
+  useEffect(() => {
+    console.log("her zaman calisir");
+  });
+  useEffect(() => {
+    console.log("ilk render edildigine calisir");
+  }, []);
+  useEffect(() => {
+    console.log(
+      "ilk render edildigine ve FirstName degeri degistiginde calisir",
+    );
+  }, [firstName]);
 
   return (
     <>
-
+      <div>{firstName}</div>
+      <div>
+        <button onClick={() => setFirstName("aa")}>change name</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
